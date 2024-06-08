b7570a35c3e3791f0c1a309881b76178b8fb7a72
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-06-08 19:03:45 +0200

    Mise à jour des dépendances. Refactorisation du composant `Navbar`

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,19 +1,58 @@
 import logo from "../../assets/img/argentBankLogo.png";
-import { NavLink } from "react-router-dom";
+import { LuLogOut } from "react-icons/lu";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
+import { logoutUser } from "../../store/userSlice";
 
 const Navbar = () => {
+
+    const dispatch = useDispatch();
+
+    const loginStore = useSelector((state) => state.login);
+
+    const token = useSelector((state) => state.login.userToken);
+
+    const handleRedirection = () => {
+        localStorage.removeItem("token");
+        dispatch(logoutUser());
+    };
+
     return (
         <nav className="main-nav">
             <NavLink className="main-nav-logo" to="/">
-                <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo"/>
+                <img 
+                    className="main-nav-logo-image" 
+                    src={logo} 
+                    alt="Argent Bank Logo"
+                />
                 <h1 className="sr-only">Argent Bank</h1>
             </NavLink>
 
             <div>
-                <NavLink className="main-nav-item" to="/SignInPage">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                </NavLink>
+                <div className="login">
+                    {loginStore &&
+                        loginStore.userProfil &&
+                        loginStore.userProfil.userName && (
+                            <Link to="/user" className="userName">
+                                <i className="fa fa-user-circle"></i>
+                                <p>{loginStore.userProfil.userName}</p>
+                            </Link>
+                        )}
+                    {token ? (
+                        <NavLink 
+                            className="main-nav-item" 
+                            to="/" 
+                            onClick={handleRedirection}>
+                            <LuLogOut />
+                            Sign Out
+                        </NavLink>
+                    ) : (
+                        <NavLink className="main-nav-item" to="/SignInPage">
+                            <i className="fa fa-user-circle"></i>
+                            Sign In
+                        </NavLink>
+                    )}
+                </div>
             </div>
         </nav>
     );
