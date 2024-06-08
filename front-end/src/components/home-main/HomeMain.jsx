4196756929d1f63f667d52ee2ede4a321d5d492a
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-06-08 18:06:35 +0200

    Réinstallation de react-redux. Refactorisation du composant `HomeMain` et modification du fichier `features.json` avec modification des chemins d'acces pour les images

--- front-end/src/components/home-main/HomeMain.jsx
+++ front-end/src/components/home-main/HomeMain.jsx
@@ -1,11 +1,24 @@
-import Feature from "../features/Features";
+import Features from "../features/Features";
 import Hero from "../hero/Hero";
+import featuresData from "../../data/features.json";
 
 const HomeMain = () => {
+    const features = featuresData.features;
     return (
         <div>
             <Hero />
-            <Feature />
+            <section className="features">
+                <h2 className="sr-only">Features</h2>
+                {features.map((features, index) => (
+                    <Features
+                        key={"feature" + index}
+                        image={features.image}
+                        alt={features.alt}
+                        title={features.title}
+                        paragraph={features.paragraph}
+                    />
+                ))}            
+            </section>
         </div>
     );
 };
